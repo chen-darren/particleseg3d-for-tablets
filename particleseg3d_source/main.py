b20045eb86c4c513033e7d8b8b0d74aa5b716cbb
chen-darren <chen.yihsu.03@gmail.com>
2025-02-21 09:10:05 -0500

    Newest tablet

--- particleseg3d_source/main.py
+++ particleseg3d_source/main.py
@@ -592,10 +592,8 @@ if __name__ == "__main__":
     # main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_tab20', name=['2_Tablet'], strategy=strategy)
     
     # main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_gen35_clar35', name=['4_GenericD12'], strategy=strategy)
-    main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_gen35_clar35', name=['5_ClaritinD12'], strategy=strategy)
+    # main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_gen35_clar35', name=['5_ClaritinD12'], strategy=strategy)
 
-    main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_gen45_clar45', name=['4_GenericD12'], strategy=strategy)
-    main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_gen45_clar45', name=['5_ClaritinD12'], strategy=strategy)
+    main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_mcc35', name=['6_30pWTMCC'], strategy=strategy)
+    main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_mcc40', name=['6_30pWTMCC'], strategy=strategy)
 
-    main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_gen50_clar50', name=['4_GenericD12'], strategy=strategy)
-    main(dir_location='refine', output_to_cloud=False, is_original_data=False, weights_tag='original_particle_seg', run_tag='pretrained_gen50_clar50', name=['5_ClaritinD12'], strategy=strategy)
