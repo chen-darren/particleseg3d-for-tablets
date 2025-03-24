c600ce3916c6037d5e251023272b748c6a0619fc
chen-darren <chen.yihsu.03@gmail.com>
2025-03-24 13:12:58 -0400

    Potentially fixing the cuda error by skipping the weight adjustment for bad steps

--- training_scripts/trainers/nnUNetTrainerV2_ParticleSeg3D_DarrenSGD.py
+++ training_scripts/trainers/nnUNetTrainerV2_ParticleSeg3D_DarrenSGD.py
@@ -1,5 +1,8 @@
 from nnunet.training.network_training.nnUNetTrainerV2_ParticleSeg3D import nnUNetTrainerV2_ParticleSeg3D
 from batchgenerators.utilities.file_and_folder_operations import *
+import torch
+from torch.cuda.amp import autocast
+from nnunet.utilities.to_torch import maybe_to_torch, to_cuda
 
 class nnUNetTrainerV2_ParticleSeg3D_DarrenSGD(nnUNetTrainerV2_ParticleSeg3D):
     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
@@ -14,3 +17,62 @@ class nnUNetTrainerV2_ParticleSeg3D_DarrenSGD(nnUNetTrainerV2_ParticleSeg3D):
         self.raw_data_dir = join("/", *nnunet_datasets_dir, "nnUNet_raw_data_base", "nnUNet_raw_data", task_name)
         self.save_every = 5
         self.initial_lr = 1e-3 # Original SGD LR is 1e-2
+        
+    def run_iteration(self, data_generator, do_backprop=True, run_online_evaluation=False):
+        """
+        gradient clipping improves training stability
+
+        :param data_generator:
+        :param do_backprop:
+        :param run_online_evaluation:
+        :return:
+        """
+        data_dict = next(data_generator)
+        data = data_dict['data']
+        target = data_dict['target']
+
+        data = maybe_to_torch(data)
+        target = maybe_to_torch(target)
+
+        if torch.cuda.is_available():
+            data = to_cuda(data)
+            target = to_cuda(target)
+
+        self.optimizer.zero_grad()
+
+        if self.fp16:
+            with autocast():
+                output = self.network(data)
+                del data
+                l = self.loss(output, target)
+
+            if do_backprop:
+                self.amp_grad_scaler.scale(l).backward()
+                self.amp_grad_scaler.unscale_(self.optimizer)
+                torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+                
+                try:
+                    self.amp_grad_scaler.step(self.optimizer)  # Attempt step
+                except RuntimeError as e:
+                    if "CUDA error" in str(e):
+                        print("CUDA error in optimizer step. Skipping this step but keeping gradients.")
+                        # Don't zero the gradients, just skip this step
+                    else:
+                        raise  # Raise other errors
+                self.amp_grad_scaler.update()
+        else:
+            output = self.network(data)
+            del data
+            l = self.loss(output, target)
+
+            if do_backprop:
+                l.backward()
+                torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+                self.optimizer.step()
+
+        if run_online_evaluation:
+            self.run_online_evaluation(output, target)
+
+        del target
+
+        return l.detach().cpu().numpy()
