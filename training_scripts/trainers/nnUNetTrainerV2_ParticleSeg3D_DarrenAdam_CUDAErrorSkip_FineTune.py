bf6f2e3239c84b2bedecf76ecc80c8751b0a24a3
Darren Chen <chen.yihsu.03@gmail.com>
2025-04-01 13:46:10 -0400

    Fine tuning trainers

---
+++ training_scripts/trainers/nnUNetTrainerV2_ParticleSeg3D_DarrenAdam_CUDAErrorSkip_FineTune.py
@@ -0,0 +1,11 @@
+from nnunet.training.network_training.nnUNetTrainerV2_ParticleSeg3D_DarrenAdam_CUDAErrorSkip import nnUNetTrainerV2_ParticleSeg3D_DarrenAdam_CUDAErrorSkip
+
+class nnUNetTrainerV2_ParticleSeg3D_DarrenAdam_CUDAErrorSkip_FineTune(nnUNetTrainerV2_ParticleSeg3D_DarrenAdam_CUDAErrorSkip):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        
+        # Revised max epochs and initial learning rate for fine tuning
+        self.max_num_epochs = 500 # 1/2 the max number of epochs of nnUNetTrainerV2_ParticleSeg3D_DarrenAdam_CUDAErrorSkip
+        self.inital_lr = 7.5e-6 # 1/4 the initial learnging rate of nnUNetTrainerV2_ParticleSeg3D_DarrenAdam_CUDAErrorSkip
---
+++ training_scripts/trainers/nnUNetTrainerV2_ParticleSeg3D_DarrenSGD_CUDAErrorSkip_FineTune.py
@@ -0,0 +1,11 @@
+from nnunet.training.network_training.nnUNetTrainerV2_ParticleSeg3D_DarrenSGD_CUDAErrorSkip import nnUNetTrainerV2_ParticleSeg3D_DarrenSGD_CUDAErrorSkip
+
+class nnUNetTrainerV2_ParticleSeg3D_DarrenSGD_CUDAErrorSkip_FineTune(nnUNetTrainerV2_ParticleSeg3D_DarrenSGD_CUDAErrorSkip):
+    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+                 unpack_data=True, deterministic=True, fp16=False):
+        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
+                         deterministic, fp16)
+        
+        # Revised max epochs and initial learning rate for fine tuning
+        self.max_num_epochs = 500 # 1/2 the max number of epochs of nnUNetTrainerV2_ParticleSeg3D_DarrenSGD_CUDAErrorSkip
+        self.inital_lr = 2.5e-4 # 1/4 the initial learnging rate of nnUNetTrainerV2_ParticleSeg3D_DarrenSGD_CUDAErrorSkip
